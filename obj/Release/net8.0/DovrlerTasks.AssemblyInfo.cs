649221bbe4e2da5f5d896badb3a3115ec607d1f9
Hasan Abbasov <abbasovhsn05@gmail.com>
2024-06-04 19:53:48 +0400

    dovrler for do-while and foreach are there

--- obj/Release/net8.0/DovrlerTasks.AssemblyInfo.cs
+++ obj/Release/net8.0/DovrlerTasks.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DovrlerTasks")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466050a9640797f916b2eb199a96c802d39c71e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DovrlerTasks")]
